7704e4c9eebf477fda1e59199711edda8212375c
Simon Kiss <skiss@Simons-MacBook-Pro.local>
2021-03-17 12:47:11 -0400

    First commit

---
+++ R_Code/1_data_import.R
@@ -0,0 +1,10 @@
+#load libraries
+library(tidyverse)
+library(labelled)
+library(haven)
+library(here)
+
+#Load data with haven
+waterloo<-read_sav(here("data/WRMS2021_autotest.sav"))
+#Get variable labels
+var_label(waterloo)
